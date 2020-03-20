7f90db89ef1a90cc0658e4280a28d94294236473
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-03-20 14:13:30 -0400

    First commit, putting everything on github

---
+++ projects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=30)
+    website = models.TextField()
+    image = models.FilePathField(path='/img')
